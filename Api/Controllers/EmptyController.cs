e692afd656d58d85cdab6079d982433589233ea8
macholewka <bumbumeq@gmail.com>
2021-05-24 21:14:37 +0200

    Clean up

---
+++ Api/Controllers/EmptyController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EmptyController : ControllerBase
+    {
+        public EmptyController()
+        {
+
+        }
+
+        [HttpGet]
+        public void Get()
+        {
+        }
+    }
+}
